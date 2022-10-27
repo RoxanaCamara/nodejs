cb5d042f066546bbc972a838a41f0ad25988a8b4
RoxanaCamara <roxana.camara07@gmail.com>
2022-10-27 01:22:28 -0300

    feat: upload funciona

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const loadFiles = async (req, res = response) => {
+const loadFiles = (req, res = response) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         res.status(400).json({ msg: 'No files were uploaded.'});
@@ -15,11 +15,10 @@ const loadFiles = async (req, res = response) => {
     const { archivo } = req.files;
     const uploadPath = path.join( __dirname, '../uploads/', archivo.name);
 
-    archivo.mv(uploadPath, function (err)  {
+    archivo.mv(uploadPath,  (err) => {
         if (err) {
-            return res.status(500).json(err);
+            return res.status(500).json({ msg: err });
         }
-
         res.json( {  msg: 'File uploaded to ' + uploadPath  });
     })
 }
