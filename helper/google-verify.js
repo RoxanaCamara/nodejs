68dcfde6ab0c72efb349740a7cea4d95fcbf7120
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-16 18:09:35 -0300

    feat: google sing-in

--- helper/google-verify.js
+++ helper/google-verify.js
@@ -1,16 +1,15 @@
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(process.env.CLIENT_ID);
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const verifyGoogle  = async(token='') => {
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.CLIENT_ID
-  });
-  const { name, picture, email } = ticket.getPayload();
-  return { name, picture, email }
-}
+const verifyGoogle = async (token = "") => {
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		audience: process.env.GOOGLE_CLIENT_ID,
+	});
+	const { name, picture, email } = ticket.getPayload();
+	return { name, picture, email };
+};
 
-
-module.exports ={
-    verifyGoogle
-}
+module.exports = {
+	verifyGoogle,
+};
