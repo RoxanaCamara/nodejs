1d21848a39c4c46d54350792529c9aa6a12040d8
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-17 18:59:58 -0300

    feat: trycatch in controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,76 +1,80 @@
+const { response, request, json } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { generarJWT } = require("../helper/generar-jwt");
+const { verifyGoogle } = require("../helper/google-verify");
 
-const {response, request, json} = require('express');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { generarJWT } = require('../helper/generar-jwt');
-const { verifyGoogle } = require('../helper/google-verify');
+const authPost = async (req, res = response) => {
+	try {
+		const { email, password } = req.body;
 
-const authPost =  async (req, res = response) => { 
-    
-    try {
-        const { email, password} = req.body
-
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(404).json({ msg: "User or password invalid"})
-        }
-        if(!user.state){
-            return res.status(404).json({ msg: "User or password invalid - For state"})
-        }
-        const validPassword = bcrypt.compareSync(password, user.password)
-        if(!validPassword){
-            return res.status(404).json({ msg: "Password invalid"})
-        }
-        const token = await generarJWT(user.id)  
-        res.json({ token, user});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ msg: "Comunicate Suport"})   
-    }
-}
-
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ msg: "User or password invalid" });
+		}
+		if (!user.state) {
+			return res
+				.status(404)
+				.json({ msg: "User or password invalid - For state" });
+		}
+		const validPassword = bcrypt.compareSync(password, user.password);
+		if (!validPassword) {
+			return res.status(404).json({ msg: "Password invalid" });
+		}
+		const token = await generarJWT(user.id);
+		res.json({ token, user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ msg: "Comunicate Suport" });
+	}
+};
 
 const googleSingIn = async (req, res = response) => {
-    const { id_token } = req.body
-    try {        
-        const { name, email, picture } = await verifyGoogle(id_token)  
-        let usuario = await User.findOne({ email })  
+	const { id_token } = req.body;
+	try {
+		const { name, email, picture } = await verifyGoogle(id_token);
+		let usuario = await User.findOne({ email });
 
-        if(!usuario){
-            //tengo que crearlo
-            const data = {
-                name, email, password: ':P', img:picture, google: true
-            }
-            const user = new User(data)
-            await user.save();
-        }
+		if (!usuario) {
+			//tengo que crearlo
+			const data = {
+				name,
+				email,
+				password: ":P",
+				img: picture,
+				google: true,
+			};
+			const user = new User(data);
+			await user.save();
+		}
 
-        usuario = await User.findOne({ email }) 
+		usuario = await User.findOne({ email });
 
-        if(!usuario.state){
-            //tengo que
-            res.status(401).json({
-                msg: 'Hable con el admin, usuario bloqueado'
-            }) 
-        }
-        const token = await generarJWT(usuario.id)
-        res.json({
-            usuario,
-            token
-        })
+		if (!usuario.state) {
+			//tengo que
+			res.status(401).json({
+				msg: "Hable con el admin, usuario bloqueado",
+			});
+		}
 
-    } catch (error) {
-        res.status(404).json({
-            msg: 'El token no se pudo verificar'
-        })
-    }
+		const token = await generarJWT(usuario.id);
 
-}
+		console.log(token);
+
+		res.json({
+			usuario,
+			token,
+		});
+	} catch (error) {
+		res.status(404).json({
+			msg: "El token no se pudo verificar",
+		});
+	}
+};
 const renovarToken = async (req, res = response) => {
-    const { user } = req
-    const token = await generarJWT(user.id)
-    res.json({ user, token })
-}
+	const { user } = req;
+	const token = await generarJWT(user.id);
+	res.json({ user, token });
+};
 
-module.exports = { authPost, googleSingIn, renovarToken }
+module.exports = { authPost, googleSingIn, renovarToken };
--- controllers/categories.js
+++ controllers/categories.js
@@ -1,64 +1,85 @@
-const {response, request, json} = require('express');
-const Categorie = require("../models/Categorie")
-
+const { response, request, json } = require("express");
+const Categorie = require("../models/Categorie");
 
 const postCategorie = async (req, res = response) => {
+	try {
+		const name = req.body.name.toUpperCase();
+		const categoriaDB = await Categorie.findOne({ name });
 
-    const name = req.body.name.toUpperCase()
-    const categoriaDB = await Categorie.findOne({ name })
+		if (categoriaDB) {
+			return res.status(400).json({
+				msg: `La categoria ${categoriaDB.name}, ya existe`,
+			});
+		}
+		const data = {
+			name,
+			user: req.user._id,
+		};
 
-    if(categoriaDB){
-        return res.status(400).json({
-            msg: `La categoria ${ categoriaDB.name }, ya existe`
-        })
-    }
-    const data = {
-        name, 
-        user: req.user._id
-    }
-
-    const categoria = new Categorie(data)
-    await categoria.save()
-    res.status(201).json(categoria)
-
-}
+		const categoria = new Categorie(data);
+		await categoria.save();
+		res.status(201).json(categoria);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
 
 const getCategories = async (req, res = response) => {
-    const { limit = 5, from= 0 } = req.query
-    const query = {status: true}
-    const [total, categories] = await Promise.all([
-        Categorie.find(query).populate('user').skip(Number(from)).limit(Number(limit)),
-        Categorie.countDocuments(query)])
+	const { limit = 5, from = 0 } = req.query;
+	const query = { status: true };
+	const [total, categories] = await Promise.all([
+		Categorie.find(query)
+			.populate("user")
+			.skip(Number(from))
+			.limit(Number(limit)),
+		Categorie.countDocuments(query),
+	]);
 
-    res.json({categories, total});    
-}
+	res.json({ categories, total });
+};
 
 const getCategorie = async (req, res = response) => {
-    const { id }  = req.params
-    let categorie = await Categorie.findById(id).populate('user')
-    if(!categorie){
-        return res.status(404).json({ msg: `La categoria ${categorie} buscada no existe`})
-    }
-    res.json(categorie);    
-}
+	const { id } = req.params;
+	let categorie = await Categorie.findById(id).populate("user");
+	if (!categorie) {
+		return res
+			.status(404)
+			.json({ msg: `La categoria ${categorie} buscada no existe` });
+	}
+	res.json(categorie);
+};
 
 const putCategorie = async (req, res = response) => {
-    const { id } = req.params
-    const { name } = req.body
-    const categorie = await Categorie.findByIdAndUpdate(id, {name}, {new: true}).populate('user')
+	const { id } = req.params;
+	const { name } = req.body;
+	const categorie = await Categorie.findByIdAndUpdate(
+		id,
+		{ name },
+		{ new: true }
+	).populate("user");
 
-    if(!categorie){
-        return res.status(404).json({ msg: `La categoria ${categorie} buscada no existe`})
-    }
-    res.json(categorie)
-}
+	if (!categorie) {
+		return res
+			.status(404)
+			.json({ msg: `La categoria ${categorie} buscada no existe` });
+	}
+	res.json(categorie);
+};
 
 const deleteCategorie = async (req, res = response) => {
-    const { id } = req.params
-    const categorie = await Categorie.findById(id, {status: false}, {new: true}).populate('user')
-    res.json(categorie)
-}
+	const { id } = req.params;
+	const categorie = await Categorie.findById(
+		id,
+		{ status: false },
+		{ new: true }
+	).populate("user");
+	res.json(categorie);
+};
 
 module.exports = {
-    postCategorie, getCategories, getCategorie, putCategorie, deleteCategorie
-}
+	postCategorie,
+	getCategories,
+	getCategorie,
+	putCategorie,
+	deleteCategorie,
+};
